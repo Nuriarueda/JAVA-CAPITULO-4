ac15025dd0e33f0a0be00046898c47b44aad95fd
Nuriarueda <ruedareinanuria@gmail.com>
2023-10-11 10:29:59 +0200

    capitulo 4

---
+++ Ejercicio12.java
@@ -0,0 +1,18 @@
+/* Realiza un minicuestionario con 10 preguntas tipo test sobre las asignaturas
+que se imparten en el curso. Cada pregunta acertada sumará un punto. El
+programa mostrará al final la calificación obtenida. Pásale el minicuestionario a
+tus compañeros y pídeles que lo hagan para ver qué tal andan de conocimientos
+en las diferentes asignaturas del curso. */
+import java.util.Scanner;
+public class Ejercicio12 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        int puntos = 0;
+
+        System.out.println("TEST DAM");
+        
+
+        
+        sc.close();
+    }
+}
