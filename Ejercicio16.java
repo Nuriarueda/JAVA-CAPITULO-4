c18def0014e8b106c18490064c3eed7b55724e78
Nuriarueda <ruedareinanuria@gmail.com>
2023-10-17 18:22:48 +0200

    capitulo 4

--- Ejercicio16.java
+++ Ejercicio16.java
@@ -1,3 +1,11 @@
+/* Realiza un programa que nos diga si hay probabilidad de que nuestra pareja nos
+está siendo infiel. El programa irá haciendo preguntas que el usuario contestará
+con verdadero o falso. Cada pregunta contestada como verdadero sumará 3
+puntos. Las preguntas contestadas con falso no suman puntos. */
+import java.util.Scanner;
 public class Ejercicio16 {
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        sc.close();
+    }
 }
