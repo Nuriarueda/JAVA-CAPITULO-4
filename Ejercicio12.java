208e4ef1633f3c80517b852726efaba31af320c0
Nuriarueda <ruedareinanuria@gmail.com>
2023-10-17 17:20:33 +0200

    capitulo 4

--- Ejercicio12.java
+++ Ejercicio12.java
@@ -11,11 +11,69 @@ public class Ejercicio12 {
 
         System.out.println("TEST DAM");
 
+        //TEST 1
         System.out.println("Que toca el lunes a primera:");
-        System.out.println("a)Sistemas informáticos");
-        System.out.println("b)Programación");
-        System.out.println("C)Lenguaje de Marcas");
+        System.out.println("1)Sistemas informáticos");
+        System.out.println("2)Programación");
+        System.out.println("3)Lenguaje de Marcas");
         System.out.print("Elige una opcion: ");
+        int respuesta1 = Integer.parseInt(sc.nextLine());
+
+        if (respuesta1 == 2) {
+            puntos++;
+        }
+        System.out.println("");
+        //TEST 2
+        System.out.println("Que toca el martes a primera:");
+        System.out.println("1)Sistemas informáticos");
+        System.out.println("2)Programación");
+        System.out.println("3)Lenguaje de Marcas");
+        System.out.print("Elige una opcion: ");
+        int respuesta2 = Integer.parseInt(sc.nextLine());
+
+        if (respuesta2 == 3) {
+            puntos++;
+        }
+        System.out.println("");
+        //TEST 3
+        System.out.println("Que toca el miercoles a tercera:");
+        System.out.println("1)Base de Datos");
+        System.out.println("2)Programación");
+        System.out.println("3)Lenguaje de Marcas");
+        System.out.print("Elige una opcion: ");
+        int respuesta3 = Integer.parseInt(sc.nextLine());
+
+        if (respuesta3 == 1) {
+            puntos++;
+        }
+        System.out.println("");
+        //TEST 4
+        System.out.println("Que programa se utiliza el programacion :");
+        System.out.println("1)C++");
+        System.out.println("2)JAVA");
+        System.out.println("3)Python");
+        System.out.print("Elige una opcion: ");
+        int respuesta4 = Integer.parseInt(sc.nextLine());
+
+        if (respuesta4 == 2) {
+            puntos++;
+        }
+        System.out.println("");
+        //TEST 5
+        System.out.println("Que se utiliza para los condicionales en JAVA:");
+        System.out.println("1)Do While");
+        System.out.println("2)For");
+        System.out.println("3)If");
+        System.out.print("Elige una opcion: ");
+        int respuesta5 = Integer.parseInt(sc.nextLine());
+
+        if (respuesta5 == 3) {
+            puntos++;
+        }
+        System.out.println("");
+        System.out.println("Tienes " + puntos + " puntos");
+
+
         sc.close();
     }
 }
