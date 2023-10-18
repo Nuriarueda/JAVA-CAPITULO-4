36734ac6fe16d744dc4dc3ce129e9bd2ebf39c53
Nuriarueda <ruedareinanuria@gmail.com>
2023-10-18 11:09:24 +0200

    capitulo 4

--- Ejercicio19.java
+++ Ejercicio19.java
@@ -4,6 +4,25 @@ import java.util.Scanner;
 public class Ejercicio19 {
     public static void main(String[] args) {
         Scanner sc = new Scanner (System.in);
+
+        System.out.print("Introduce un numero (maximo 5 digitos): ");
+        int n = Integer.parseInt(sc.nextLine());
+
+        int numdigitos = 0;
+
+        if (n<10){
+            numdigitos = 1;
+        } else if (n>=10 && n<100){
+            numdigitos = 2;
+        } else if (n>=100 && n<1000){
+            numdigitos = 3;
+        } else if (n>=1000 && n <10000){
+            numdigitos = 4;
+        } else if (n>=10000 && n<100000){
+            numdigitos = 5;
+        }
+
+        System.out.println("El numero " + n + " tiene " + numdigitos + " digitos");
         sc.close();
     }
 }
