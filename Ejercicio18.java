0bce04c0ce4bfe5701d1d88c2df1a44f803c6013
Nuriarueda <ruedareinanuria@gmail.com>
2023-10-18 10:45:08 +0200

    capitulo 4

---
+++ Ejercicio18.java
@@ -0,0 +1,9 @@
+/* Escribe un programa que diga cuál es la primera cifra de un número entero
+introducido por teclado. Se permiten números de hasta 5 cifras. */
+import java.util.Scanner;
+public class Ejercicio18 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        sc.close();
+    }
+}
