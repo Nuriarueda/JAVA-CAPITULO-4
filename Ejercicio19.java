9ebb32973464f03877eb3a0610dd01def3be1a84
Nuriarueda <ruedareinanuria@gmail.com>
2023-10-18 10:46:14 +0200

    capitulo 4

---
+++ Ejercicio19.java
@@ -0,0 +1,9 @@
+/* Realiza un programa que nos diga cuántos dígitos tiene un número entero que
+puede ser positivo o negativo. Se permiten números de hasta 5 dígitos. */
+import java.util.Scanner;
+public class Ejercicio19 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        sc.close();
+    }
+}
