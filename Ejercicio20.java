5342a4bdac676daf9e2bb071f23853af3062183b
Nuriarueda <ruedareinanuria@gmail.com>
2023-10-18 11:13:03 +0200

    capitulo 4

--- Ejercicio20.java
+++ Ejercicio20.java
@@ -1,3 +1,16 @@
+/* Calcula la nota de un trimestre de la asignatura Programación. El programa
+pedirá las dos notas que ha sacado el alumno en los dos primeros controles.
+Si la media de los dos controles da un número mayor o igual a 5, el alumno
+está aprobado y se mostrará la media. En caso de que la media sea un número
+menor que 5, el alumno habrá tenido que hacer el examen de recuperación que
+se califica como apto o no apto, por tanto se debe preguntar al usuario ¿Cuál
+ha sido el resultado de la recuperación? (apto/no apto). Si el resultado
+de la recuperación es apto, la nota será un 5; en caso contrario, se mantiene
+la nota media anterior. */
+import java.util.Scanner;
 public class Ejercicio20 {
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        sc.close();
+    }
 }
