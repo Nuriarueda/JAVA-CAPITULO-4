bb2a30eae2b5fd89a2f95310501608ebc3091ba5
Nuriarueda <ruedareinanuria@gmail.com>
2023-10-18 10:51:07 +0200

    capitulo 4

--- Ejercicio17.java
+++ Ejercicio17.java
@@ -4,6 +4,13 @@ import java.util.Scanner;
 public class Ejercicio17 {
     public static void main(String[] args) {
         Scanner sc = new Scanner (System.in);
+
+        System.out.print("Introduce un numero y te dire cual es el ultimo digito: ");
+        int n = Integer.parseInt(sc.nextLine());
+
+        int ultdigito = n%10;
+
+        System.out.println("El ultimo digito de " + n + " es " + ultdigito);
         sc.close();
     }
 }
