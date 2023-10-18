bb9a473ab65281960aa1d6f3f921464d10c125f8
Nuriarueda <ruedareinanuria@gmail.com>
2023-10-18 10:44:00 +0200

    capitulo 4

---
+++ Ejercicio17.java
@@ -0,0 +1,9 @@
+/* Escribe un programa que diga cuál es la última cifra de un número entero
+introducido por teclado. */
+import java.util.Scanner;
+public class Ejercicio17 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        sc.close();
+    }
+}
