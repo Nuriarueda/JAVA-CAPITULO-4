6d892f3676f302e663ad831720a06a8f0e8055ea
Nuriarueda <ruedareinanuria@gmail.com>
2023-10-18 11:03:13 +0200

    capitulo 4

--- Ejercicio18.java
+++ Ejercicio18.java
@@ -4,6 +4,24 @@ import java.util.Scanner;
 public class Ejercicio18 {
     public static void main(String[] args) {
         Scanner sc = new Scanner (System.in);
+
+        System.out.print("Introduce un numero (maximo 5 cifras): ");
+        int n = Integer.parseInt(sc.nextLine());
+        int digito1 = 0;
+
+        if (n<10) {
+            digito1 = n;
+        } else if (n>=10 && n<100){
+            digito1 = n/10;
+        } else if (n>=100 & n<1000){
+            digito1 = n/100;
+        } else if (n>=1000 && n<10000){
+            digito1 = n/1000;
+        } else if (n>=10000 && n<100000){
+            digito1 = n/10000;
+        } 
+
+        System.out.println("El primer digito de " + n + " es " + digito1);
         sc.close();
     }
 }
